56d63dea597d1dc8c498b8d3a928f37807d2787a
adilahiri <adi441994@gmail.com>
2020-06-15 16:42:57 -0500

    updated the number of genes and fixed the network config

--- get_discrete_data.R
+++ get_discrete_data.R
@@ -1,6 +1,7 @@
 get_discrete_data <- function (data,meth)
 {
-  for (iter in 1:14){
+  col_num = ncol(data)
+  for (iter in 1:col_num){
     temp <- discretize(data[,iter],method= meth,
                               breaks=3,labels = c(-1,0,1))
     data[,iter] <- as.numeric(levels(temp))[temp]
